f3b1007d744a398165acee5c9329bf948dcd3d4e
daveball <dave.mike.ball@gmail.com>
2016-12-19 22:47:08 +0000

    added  data types

--- datatypes/datatypes/Program.cs
+++ datatypes/datatypes/Program.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
+using Microsoft.SqlServer.Server;
+using Microsoft.Win32;
+
 
 namespace datatypes
 {
@@ -7,6 +13,24 @@ namespace datatypes
     {
         public static void Main(string[] args)
         {
+
+            // create a method  called  JoinStrings which takes in two strings then output the result to the console and
+            //return a string of these stings joined with a space  between them make sure it is public
+            // call JoinStrings with your first and last name
+
+
+
+            // create a method  called  Arthimetic which takes in two  numbers and returns a result  of a  mathematical
+            //function which is also sent a a single character the result sould be  printed  to the console
+            // call  arithmetic with an example of each type of calculation
+
+            //Create a method called WriteStringArray which writes the contents of an array to the colsole.
+            // call WriteStringArray with an array containign 3 strings
+
+
+
         }
+
+
     }
 }
---
+++ datatypes/datatypes/TestDataTypes.cs
@@ -0,0 +1,144 @@
+﻿using NUnit;
+using System;
+using System.Collections.Generic;
+using Microsoft.Win32;
+using NUnit.Framework;
+
+
+/*
+ public static string JoinStrings(String firstString,String secondString)
+        {
+            var resultString = firstString + ' ' + secondString;
+            Console.WriteLine(resultString);
+            return resultString;
+        }
+
+        public static int Arithmetic(int A, int B, char Operator)
+        {
+            var result =0;
+            if (Operator == '+')
+            {
+                result = A + B;
+
+
+            }
+            if (Operator == '-')
+            {
+                result = A - B;
+
+
+            }
+            if (Operator == '*')
+            {
+                result = A * B;
+
+
+            }
+            if (Operator == '/')
+            {
+                result = A / B;
+
+
+            }
+            Console.Write(result);
+            return result;
+        }
+        public static void WriteStringArray(string[] myStrings)
+        {
+            foreach (var text in myStrings)
+            {
+                Console.Write(text +" ");
+            }
+
+        }
+
+*/
+namespace datatypes
+{
+
+    [TestFixture]
+    public class ExpectedExceptionTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void HandlesArgumentExceptionAsType()
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    [TestFixture]
+    public class TestDataTypes
+    {
+        [Test]
+        public void TestTestingIsWorking()
+        {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void TestJoinStrings()
+        {
+            var myResult = Program.JoinStrings("Dave", "Ball");
+            Assert.AreEqual(myResult, "Dave Ball");
+            //Assert.AreEqual(this.GetLastTextWrittenInConsole,"Dave Ball" );
+        }
+
+        [Test]
+        public void TestArithmeticAddition()
+        {
+            int myResult = Program.Arithmetic( 1, 2 ,'+');
+            Assert.That(myResult, Is.EqualTo(3));
+            //Assert.AreEqual(this.GetLastTextWrittenInConsole,"3" );
+        }
+
+        [Test]
+        public void TestArithmeticSubtraction()
+        {
+            int myResult = Program.Arithmetic( 1, 2 ,'-');
+            Assert.That(myResult, Is.EqualTo(-1));
+           // Assert.AreEqual(this.GetLastTextWrittenInConsole,"-1" );
+        }
+
+        [Test]
+        public void TestArithmeticMultiply()
+        {
+            int myResult = Program.Arithmetic( 4, 2 ,'*');
+            Assert.That(myResult, Is.EqualTo(8));
+            //Assert.AreEqual(this.GetLastTextWrittenInConsole,"8" );
+        }
+
+        [Test]
+        public void TestArithmeticDivision()
+        {
+            int myResult = Program.Arithmetic( 4, 2 ,'/');
+            Assert.That(myResult, Is.EqualTo(2));
+            //Assert.AreEqual(this.GetLastTextWrittenInConsole,"2" );
+        }
+
+
+
+        [Test]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestArithmeticDivisionByZero()
+        {
+            int myResult = Program.Arithmetic( 4, 0 ,'/');
+
+
+        }
+
+        [Test]
+        public void TestWriteStringArray()
+        {
+            string[] myStrings = new string[3];
+            myStrings[0] = "hello";
+            myStrings[1] = "world";
+            myStrings[2] = "!!!";
+            //Assert.AreEqual(this.GetLastTextWrittenInConsole, "hello world !!! ");
+
+
+        }
+
+
+    }
+}
