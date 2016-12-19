d7639d5ed01563a67fe4cfcba36446ed42c7c0ff
daveball <dave.mike.ball@gmail.com>
2016-12-19 21:25:14 +0000

    added  data types

---
+++ datatypes/datatypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace datatypes
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
